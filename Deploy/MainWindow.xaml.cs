f5e1213dd39d9e8346e1c8c9eaba8f54ef369c39
Andrei Vaida <andrei.vaida@itiviti.com>
2024-12-05 14:18:51 +0200

    Create basic UI

---
+++ Deploy/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace Deploy
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void OnBuild(object sender, RoutedEventArgs e)
+        {
+            // TODO
+        }
+        public void OnOpenProjectLocation(object sender, RoutedEventArgs e)
+        {
+            // TODO
+        }
+
+        public void OnOpenPlatformLocation(object sender, RoutedEventArgs e)
+        {
+            // TODO
+        }
+    }
+}
